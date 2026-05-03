c4d70772a0b2fdb6c9850e6ccddc42d836a59275
TheSoumenMondal <thesoumenmondal50@gmail.com>
2026-05-03 12:07:57 +0530

    add rate limiter

--- src/socket.ts
+++ src/socket.ts
@@ -4,6 +4,8 @@ import { Server } from "socket.io";
 import { pubClient, redisClient, subClient } from "./configs/redis.js";
 import { logger } from "./loggers/logger.js";
 
+const RATE_LIMIT_MS = 500;
+
 function initSocketServer(server: http.Server) {
 	const io = new Server(server);
 	io.adapter(createAdapter(pubClient, subClient));
@@ -11,13 +13,28 @@ function initSocketServer(server: http.Server) {
 	io.on("connection", (socket) => {
 		logger.info(`New client connected: ${socket.id}`);
 
+		const lastEmitTime = new Map<number, number>();
+
 		socket.on("client:state:change", async (data) => {
 			const cookies = socket.handshake.headers.cookie
-				? Object.fromEntries(socket.handshake.headers.cookie.split(';').map(c => c.trim().split('=')))
+				? Object.fromEntries(
+						socket.handshake.headers.cookie
+							.split(";")
+							.map((c) => c.trim().split("=")),
+					)
 				: {};
 			if (!cookies.auth_token) {
 				return;
 			}
+
+			const now = Date.now();
+			const last = lastEmitTime.get(data.i) ?? 0;
+			if (now - last < RATE_LIMIT_MS) {
+				logger.warn(`Rate limited: socket=${socket.id} checkbox=${data.i}`);
+				return;
+			}
+			lastEmitTime.set(data.i, now);
+
 			await redisClient.hset("checkbox_state", data.i, String(data.state));
 			io.emit("server:state:change", data);
 		});
